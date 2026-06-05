323f201e70ecc78eea92899b959866a00e470227
kachi-1105 <odecodesu123@gmail.com>
2026-06-05 15:36:23 +0900

    package-lockの再生成

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // GitHub Pages の公開 URL（例: https://your-username.github.io）
   site: 'https://kachi-1105.github.io',
-  // リポジトリ名（例: リポジトリ名が kachi-blog なら '/kachi-blog'）
-  // カスタムドメインを使う場合や username.github.io リポジトリの場合は削除してください
   base: '/<kachi-blog>',
 });
