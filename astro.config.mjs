026e7a919d26e6beb770f36c8085ff92314fe824
kachi-1105 <odecodesu123@gmail.com>
2026-06-05 15:16:32 +0900

    second commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   // GitHub Pages の公開 URL（例: https://your-username.github.io）
-  site: 'https://<your-github-username>.github.io',
+  site: 'https://kachi-1105.github.io',
   // リポジトリ名（例: リポジトリ名が kachi-blog なら '/kachi-blog'）
   // カスタムドメインを使う場合や username.github.io リポジトリの場合は削除してください
-  base: '/<your-repo-name>',
+  base: '/<kachi-blog>',
 });
